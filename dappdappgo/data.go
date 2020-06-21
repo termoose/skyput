fb3a6b956783e0fef95464acdc09c6ee4943ee48
Ole Andre Birkedal <birkedal@extab.net>
2020-06-21 14:53:45 +0200

    Removed comment

--- dappdappgo/data.go
+++ dappdappgo/data.go
@@ -2,10 +2,6 @@ package dappdappgo
 
 const portalUrl = "https://dappdappgo.coolhd.hu/api/add_skylink.php"
 
-// {'error':'wrong_skylink','msg':'The submitted skylink is invalid,
-// please submit 'sia:{46 char}' or '{46 char}' via POST.
-// Please do NOT submit urls with portal domains.'}
-
 type Response struct {
 	Error string `json:"error"`
 	Msg   string `json:"msg"`
